3ebf803ca1be45b705023e7ef849c98184575b63
Isaac Vallee <isaacvallee@gmail.com>
2017-10-10 19:01:52 +0000

    assigned stores to instance vars

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -2,8 +2,8 @@ require_relative '../setup'
 
 puts "Exercise 1"
 puts "----------"
-burnaby = Store.create(annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-richmond = Store.create(annual_revenue: 1260000, womens_apparel: true)
-gastown = Store.create(annual_revenue: 190000, mens_apparel: true)
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true)
 
 puts "There are #{Store.count} stores"
