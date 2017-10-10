2f99508c6d5c57ef72ec134cab8286b97abc0a49
Isaac Vallee <isaacvallee@gmail.com>
2017-10-10 20:10:27 +0000

    create employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Bob", last_name: "Loblaw", hourly_rate: 23)
+@store1.employees.create(first_name: "Tim", last_name: "Blimblaw", hourly_rate: 18)
+@store1.employees.create(first_name: "George", last_name: "Forelorge", hourly_rate: 18)
+@store1.employees.create(first_name: "Fred", last_name: "Bedson", hourly_rate: 21)
+
+
+@store2.employees.create(first_name: "Florb", last_name: "Gorbtorb", hourly_rate: 18)
+@store2.employees.create(first_name: "John", last_name: "Flonflon", hourly_rate: 20)
+@store2.employees.create(first_name: "Fig", last_name: "Digpig", hourly_rate: 26)
+
+binding.pry
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ActiveRecord::Base
-
+  belongs_to :store,
+  validates :fist_name, presence: true,
+  
 end
