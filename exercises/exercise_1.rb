97d0c6dc565b3aeecf3e109d04cb8feffaa5683d
Isaac Vallee <isaacvallee@gmail.com>
2017-10-10 18:52:29 +0000

    create stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -2,5 +2,8 @@ require_relative '../setup'
 
 puts "Exercise 1"
 puts "----------"
+burnaby = Store.create(annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(annual_revenue: 1260000, womens_apparel: true)
+gastown = Store.create(annual_revenue: 190000, mens_apparel: true)
 
-# Your code goes below here ...
+puts "There are #{Store.count} stores"
